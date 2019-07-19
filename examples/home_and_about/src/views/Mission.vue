2437fe5803f8b8cd2497ef55ba8f36f91e915892
Raymond Camden <raymondcamden@gmail.com>
2019-07-19 10:06:29 -0500

    more updates

---
+++ examples/home_and_about/src/views/Mission.vue
@@ -0,0 +1,16 @@
+<!-- http://www.jonhaworth.com/toys/mission-statement-generator -->
+<template>
+	<div>
+		It is our business to seamlessly utilise cost-effective leadership skills to meet our clients' needs.
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ examples/ray_prototype.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>This is {{name}}</h1>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: 'Raymond'
+		}
+	}
+}
+</script>
+
+<style>
+h1 {
+	color: red;
+}
+</style>
